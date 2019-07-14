48f3fab59cf7cb8cbb46d5bb394c6780ec2ecd5d
dalsontws <dalsontws@gmail.com>
2019-07-14 11:37:17 +0800

    Edit Error Page

--- src/views/Errors.js
+++ src/views/Errors.js
@@ -5,9 +5,12 @@ const Errors = () => (
   <Container fluid className="main-content-container px-4 pb-4">
     <div className="error">
       <div className="error__content">
-        <h2>500</h2>
-        <h3>Something went wrong!</h3>
-        <p>There was a problem on our end. Please try again later.</p>
+        <h2>Hello!</h2>
+        <h3>Send Us A Review!</h3>
+        <p>
+          Do provide us with your honest feedback and you will improve your
+          experience!
+        </p>
         <Button pill>&larr; Go Back</Button>
       </div>
     </div>
